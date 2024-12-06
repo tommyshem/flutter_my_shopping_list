4991c3e22617c7c2e7d3b2a5feeb0726aeb7ca66
tommy shem <tommy@test.com>
2024-12-06 14:10:35 +0000

    constant values

---
+++ lib/constants.dart
@@ -0,0 +1,5 @@
+const String appName = 'My Shopping List';
+
+// sign in pages
+const String signInPageTitle = "Sign In for Dad's Shopping List App";
+const String signInButtonText = 'Sign In Anonymously';
