9fe15b793e1dc6f8c44944d37519ba667771bf62
tommy shem <tommy@test.com>
2024-12-06 14:04:06 +0000

    fire store

---
+++ lib/services/firebase/firestore_db.dart
@@ -0,0 +1,123 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import '../../data_,models/shop_item_model.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class FireStoreDB {
+  // database
+  final _fireStoreDB = FirebaseFirestore.instance;
+  // shopping List
+  final List<ShopItem> _shoppingItems = [];
+  // firestore collection name for shopping list
+  final String _shoppingList = "shopping_list";
+  final String _inventoryList = "inventory_list";
+  String userName = "";
+  String userImagePath = "";
+
+  FireStoreDB() {
+    // allow offline data
+    _fireStoreDB.settings = const Settings(persistenceEnabled: true);
+  }
+
+  String getUserName() {
+    return userName;
+  }
+
+  setUserName(String user) {
+    userName = user;
+  }
+
+  // shopping List table functions below
+
+  Future<List<ShopItem>> readAllShoppingList() async {
+    final snapshot = await _fireStoreDB.collection(_shoppingList).get();
+    final shoppingItemsList =
+        snapshot.docs.map((e) => ShopItem.fromSnapshot(e)).toList();
+    _shoppingItems.clear();
+    _shoppingItems.addAll(shoppingItemsList);
+    debugPrint("readallshoppinglist function called");
+    return _shoppingItems;
+  }
+
+  int getShoppingListLength() {
+    debugPrint("shopping list length => ${_shoppingItems.length}");
+    return _shoppingItems.length;
+  }
+
+  getShoppingItemAtIndex(int index) {
+    return _shoppingItems[index];
+  }
+
+  deleteShoppingItem(String item) {
+    deleteItem(item, _shoppingList);
+  }
+
+  deleteItem(String item, String collectionName) {
+    _fireStoreDB.collection(collectionName).doc(item).delete().then(
+          (doc) => debugPrint("Document deleted"),
+          onError: (e) => debugPrint("Error updating document ${e.toString()}"),
+        );
+  }
+
+  Future<ShopItem> findShoppingItem(String name) async {
+    final snapshot = await _fireStoreDB
+        .collection(_shoppingList)
+        .where("name", isEqualTo: name)
+        .get();
+    final shoppingItem =
+        snapshot.docs.map((e) => ShopItem.fromSnapshot(e)).single;
+    return shoppingItem;
+  }
+
+  addShoppingItem(ShopItem shopItem) {
+    writeShoppingItem(shopItem);
+  }
+
+  writeShoppingItem(ShopItem shopItem) {
+    _fireStoreDB
+        .collection(_shoppingList)
+        .add(shopItem.toJson())
+        .then(
+          (DocumentReference doc) => debugPrint(
+              'Shopping Item - DocumentSnapshot added with ID: ${doc.id}'),
+        )
+        .onError(
+          (error, stackTrace) => debugPrint(
+            error.toString(),
+          ),
+        );
+  }
+
+  // items table functions below
+  Future<List<ShopItem>> readAllInventoryList() async {
+    final snapshot = await _fireStoreDB.collection(_inventoryList).get();
+    final shoppingItemsList =
+        snapshot.docs.map((e) => ShopItem.fromSnapshot(e)).toList();
+    return shoppingItemsList;
+  }
+
+  writeInventoryItem(ShopItem shopItem) {
+    _fireStoreDB.collection(_inventoryList).add(shopItem.toJson()).then(
+          (DocumentReference doc) =>
+              debugPrint('DocumentSnapshot added with ID: ${doc.id}'),
+        );
+  }
+
+  findInventoryItem() {
+    // Create a reference to the cities collection
+    final itemsRef = _fireStoreDB.collection(_inventoryList);
+
+// Create a query against the collection.
+    itemsRef.where("name", isEqualTo: "orange").get().then(
+          (res) => {
+            for (var doc in res.docs)
+              {debugPrint("${doc.id} => ${doc.data()}")},
+          },
+          onError: (e) => debugPrint("Error completing: ${e.toString()}"),
+        );
+  }
+
+  deleteInventoryItem(String item) {
+    deleteItem(item, _inventoryList);
+  }
+}
---
+++ lib/services/firebase/login.dart
@@ -0,0 +1,81 @@
+import "package:firebase_auth/firebase_auth.dart";
+import 'package:flutter/material.dart';
+
+// firebase real database functions
+class Anonymously {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  // sign in anonymously
+  Future signInAnonymously() async {
+    try {
+      UserCredential result = await _auth.signInAnonymously();
+      User? user = result.user;
+      return user;
+    } catch (e) {
+      debugPrint(
+        e.toString(),
+      );
+      return null;
+    }
+  }
+
+  // register with name and password
+
+  // sign out
+}
+
+// check state of user - ie signed in or not
+Future checkIfSignedIn() async {
+  FirebaseAuth.instance.authStateChanges().listen((User? user) {
+    if (user == null) {
+      debugPrint('User is currently signed out!');
+    } else {
+      debugPrint('User is signed in!');
+    }
+  });
+}
+
+// create a new user on the firebase database
+Future<UserCredential?> createUser(emailAddress, password) async {
+  UserCredential? credential;
+  try {
+    credential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+      email: emailAddress,
+      password: password,
+    );
+  } on FirebaseAuthException catch (e) {
+    if (e.code == 'weak-password') {
+      debugPrint('The password provided is too weak.');
+    } else if (e.code == 'email-already-in-use') {
+      debugPrint('The account already exists for that email.');
+    }
+  } catch (e) {
+    debugPrint(e.toString());
+  }
+  return credential;
+}
+
+// sign in with email and password to the firebase database
+Future<bool?> signInUser(String? emailAddress, String? password) async {
+  try {
+    //  emailAddress != null ? "" : "";
+    final credential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: emailAddress as String, password: password as String);
+  } on FirebaseAuthException catch (e) {
+    if (e.code == 'user-not-found') {
+      debugPrint('No user found for that email.');
+      return false;
+    } else if (e.code == 'wrong-password') {
+      debugPrint('Wrong password provided for that user.');
+      return false;
+    } else {
+      debugPrint(e.code.toString());
+      return false;
+    }
+  }
+  return true;
+}
+
+// sign out of the firebase database
+signOut() async {
+  await FirebaseAuth.instance.signOut();
+}
